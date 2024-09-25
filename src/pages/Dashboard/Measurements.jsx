103c76757ae455df6d7f6e6d7e60297c9b637236
rogerpww2002 <rogealg@gmail.com>
2024-09-25 16:21:11 -0600

    Ajuste automático de tarjetas pestaña de mediciones para formatos md y menores ajuste de una sola página

--- src/pages/Dashboard/Measurements.jsx
+++ src/pages/Dashboard/Measurements.jsx
@@ -8,31 +8,26 @@ import PowerFactor from "./cards/PowerFactor";
 import Timestamp from "./cards/Timestamp";
 
 
-export default function Measurments (){
-    return (
-        <Box sx={{ flexGrow: 1}}>
-            {/* First row of 2 cards */}
-            <Grid container spacing={3}>
-            <Grid size={6}>
-                <Current/>
-              </Grid>
-    
-              <Grid size={6}>
-                <AccumulatedConsumption/>
-              </Grid>
+export default function Measurments() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={3}>
+        {/* Primera fila */}
+        <Grid size={{ xs: 12, sm: 12, md:12, lg:6 }}>
+          <Current />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 12, md: 12, lg:6 }}>
+          <AccumulatedConsumption />
+        </Grid>
 
-             {/* Second row of 2 cards */}
-              <Grid size={6}>
-                <PowerFactor />
-              </Grid>
-    
-              <Grid size={6}>
-                <Timestamp />
-              </Grid>
-    
-            
-            </Grid>
-        </Box>
-      );
-    };
-    
+        {/* Segunda fila */}
+        <Grid size={{ xs: 12, sm: 12, md: 12, lg:6 }}>
+          <PowerFactor />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 12, md: 12, lg:6 }}>
+          <Timestamp />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
