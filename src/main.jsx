76a7d2b29bb5aee72d4923c53b4f97a06bc23596
Arturo Vargas Cuevas <avargas2705@outlook.com>
2024-09-28 17:31:32 -0600

    testing new theme feature, so splitted the code

--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Vanilla imports
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { BrowserRouter } from 'react-router-dom';
+// MUI imports
+import { BrowserRouter } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme.js";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
+function MainApp() {
+  const [theme, colorMode] = useMode();
 
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-
-
-const themeOptions = {
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#69B2A6',
-    },
-    secondary: {
-      main: '#F2A007',
-    },
-    background: {
-      default: '#111A2B',
-      paper: '#1E2940',
-    },
-  },
-};
-
-// Create a theme with both light and dark modes
-const theme = createTheme({
-  palette: {
-    mode: 'light', // Default light mode
-    primary: {
-      main: '#69B2A6',
-    },
-    secondary: {
-      main: '#F2A007',
-    },
-    background: {
-      default: '#eef2f6',
-      paper: '#ffffff',
-    },
-  },
-  colorSchemes: {
-    dark: {
-      palette: themeOptions.palette,
-    },
-  },
-});
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <MainApp />
   </StrictMode>
 );
